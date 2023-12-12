b98af1e49ac9f76d6b458ec4ef427c7dec9f2799
Rafael Bolaños <rafael.bolanos@thekag.com>
2023-12-12 18:23:50 -0500

    passing parameters to methods

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -3,9 +3,11 @@
 		<h2>{{ appTitle }}</h2>
 		<h3>{{ counterData.title }}</h3>
 		<div>
-			<button @click="decreaseCounter" class="btn">-</button>
+			<button @click="decreaseCounter(2)" class="btn">--</button>
+			<button @click="decreaseCounter(2)" class="btn">-</button>
 			<span class="counter">{{ counterData.count }}</span>
-			<button @click="increaseCounter" class="btn">+</button>
+			<button @click="increaseCounter(1, $event)" class="btn">+</button>
+			<button @click="increaseCounter(2)" class="btn">++</button>
 		</div>
 
 		<div class="edit">
@@ -20,20 +22,18 @@ import { reactive } from 'vue'
 
 const appTitle = 'My Amazing Counter App';
 
-// const counter = ref(0),
-// 			counterTitle = ref('My Counter:')
-
 const counterData = reactive({
 	count: 0,
 	title: 'My Counter:'
 })
 
-const increaseCounter = () =>{
-	counterData.count++;
+const increaseCounter = (amount, event) =>{
+	// console.log(event);
+	counterData.count += amount;
 }
 
-const decreaseCounter = () =>{
-	counterData.count--;
+const decreaseCounter = amount =>{
+	counterData.count -= amount;
 }
 
 </script>
