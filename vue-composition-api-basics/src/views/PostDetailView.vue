6127e745d539a5ad33298c6d5862fe4bd8c8eefb
Rafael Bolaños <rafael.bolanos@thekag.com>
2023-12-20 14:42:44 -0500

    Setting up posts page using router and router params

---
+++ vue-composition-api-basics/src/views/PostDetailView.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="post-detail">
+		<h1>This is a post-page</h1>
+		<p>Display the content of the post with Id: {{ $route.params.id }}</p>
+		<p><RouterLink to="/posts">&lt; Back</RouterLink></p>
+	</div>
+</template>
+
+<script setup>
+/**
+ * Imports
+ */
+ import { vAutofocus } from '@/directives/vAutofocus'
+
+</script>
