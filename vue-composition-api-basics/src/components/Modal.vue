67f87a40bd4b8eb95f6d4fa2e0eef588ed511239
Rafael Bolaños <rafael.bolanos@thekag.com>
2023-12-21 12:12:57 -0500

    Child components

---
+++ vue-composition-api-basics/src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+	<Teleport to=".modal-container">
+			<div class="modal">
+				<h1>This is a modal!</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure inventore sapiente, expedita, dolores quibusdam corporis eveniet, accusamus cumque neque unde quae reprehenderit in facilis eaque optio excepturi omnis. Totam, obcaecati?</p>
+				<button>Hide modal</button>
+			</div>
+	</Teleport>
+</template>
+
+<style scoped>
+.modal{
+	background: beige;
+	padding: 10px;
+	color: black;
+	position: absolute;
+	left: 0;
+	bottom: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 1;
+}
+</style>
--- vue-composition-api-basics/src/views/ModalsView.vue
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,14 +2,8 @@
 	<div class="modals">
 		<h1>Modals</h1>
 		<button @click="showModal = true">Show modal</button>
-		<Teleport to=".modal-container">
-			<div v-if="showModal" class="modal">
-				<h1>This is a modal!</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure inventore sapiente, expedita, dolores quibusdam corporis eveniet, accusamus cumque neque unde quae reprehenderit in facilis eaque optio excepturi omnis. Totam, obcaecati?</p>
-				<button @click="showModal = false">Hide modal</button>
-			</div>
-	</Teleport>
 	</div>
+	<Modal v-if="showModal"></Modal>
 </template>
 
 <script setup>
@@ -17,22 +11,10 @@
  * Imports
  */
 import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
 
 /**
  * Modals
  */
 	const showModal = ref(false);
 </script>
-
-<style scoped>
-.modal{
-	background: beige;
-	padding: 10px;
-	color: black;
-	position: absolute;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	z-index: 1;
-}
-</style>
