7b89ac6cc4cb283da4ebdeb4239abf2fee978cb2
Rafael Bolaños <rafael.bolanos@thekag.com>
2023-12-21 14:36:25 -0500

    Creating and using a composable.

---
+++ vue-composition-api-basics/src/use/useCounter.js
@@ -0,0 +1,33 @@
+import { reactive, computed, watch, nextTick } from 'vue'
+
+export function useCounter(){
+	/**
+ * Counter 
+ */
+	const counterData = reactive({
+		count: 0,
+		title: 'My Counter:'
+	});
+
+	watch(() => counterData.count, (newCount, oldCount) =>{
+		if(newCount === 20) alert('Way to go made it to 20.');
+	});
+
+	const oddOrEven = computed(() => {
+		return counterData.count % 2 === 0 ? 'even' : 'odd';
+	});
+
+	const increaseCounter = async (amount, event) =>{
+		// console.log(event);
+		counterData.count += amount;
+		await nextTick();
+		console.log("Do something when counter has updated in the log");
+		
+	}
+
+	const decreaseCounter = amount =>{
+		counterData.count -= amount;
+	}
+
+	return { counterData, oddOrEven, increaseCounter, decreaseCounter }
+}
--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -20,8 +20,9 @@
 </template>
 
 <script setup>
-import { reactive, computed, watch, onMounted, ref, nextTick } from 'vue'
+import { onMounted, ref } from 'vue'
 import { vAutofocus } from '@/directives/vAutofocus'
+import { useCounter } from '@/use/useCounter'
 
 /**
  * App title
@@ -37,34 +38,10 @@ import { vAutofocus } from '@/directives/vAutofocus'
 		console.log(`The app title is ${appTitleRef.value.offsetWidth} px wide!`);
 	});
 
-/**
- * Counter 
- */
-
-	const counterData = reactive({
-		count: 0,
-		title: 'My Counter:'
-	});
-
-	watch(() => counterData.count, (newCount, oldCount) =>{
-		if(newCount === 20) alert('Way to go made it to 20.');
-	});
-
-	const oddOrEven = computed(() => {
-		return counterData.count % 2 === 0 ? 'even' : 'odd';
-	});
-
-	const increaseCounter = async (amount, event) =>{
-		// console.log(event);
-		counterData.count += amount;
-		await nextTick();
-		console.log("Do something when counter has updated in the log");
-		
-	}
-
-	const decreaseCounter = amount =>{
-		counterData.count -= amount;
-	}
+	/**
+	 * Counter
+	 */
+		const { counterData, decreaseCounter, increaseCounter, oddOrEven  } = useCounter();
 
 </script>
 
