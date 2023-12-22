b8aa3eb3a66450c3b5148fb305f93409e75349d2
Rafael Bolaños <rafael.bolanos@thekag.com>
2023-12-21 21:47:03 -0500

    Computed note length

--- vue-composition-api-noteballs/src/components/notes/Note.vue
+++ vue-composition-api-noteballs/src/components/notes/Note.vue
@@ -5,6 +5,9 @@
 		<div class="card-content">
 			<div class="content">
 				{{ note.content }}
+				<div class="has-text-right has-text-grey-light mt-2">
+					<small>{{ characterLength }}</small>
+				</div>
 			</div>
 		</div>
 		<footer class="card-footer">
@@ -15,6 +18,8 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
+
 /**
  * Props
  */
@@ -23,6 +28,15 @@
 			type: Object,
 			required: true
 		}
+	})
+
+	/**
+	 * Note content
+	 */
+	const characterLength = computed(() => {
+		let description = props.note.content.length > 1 ? 'Characters' : 'Character';
+
+		return `${props.note.content.length} ${description}`;
 	})
 
 </script>
