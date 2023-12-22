8805a2400f735209669bdd8ab0b39109992f1766
Rafael Bolaños <rafael.bolanos@thekag.com>
2023-12-21 21:33:39 -0500

    Add note method

--- vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -5,14 +5,19 @@
 			<div class="field">
 				<div class="control">
 					<textarea
+						v-model="newNote"
 						class="textarea"
-						placeholder="Add a new note" />
+						placeholder="Add a new note" 
+						ref="newNoteRef"/>
 				</div>
 			</div>
 	
 			<div class="field is-grouped is-grouped-right">
 				<div class="control">
-					<button class="button is-link has-background-success">
+					<button
+						class="button is-link has-background-success"
+						@click="addNote"
+						:disabled="!newNote">
 						Add New Note
 					</button>
 				</div>
@@ -47,15 +52,32 @@
 /**
  * Notes
  */
+	const newNote = ref('');
+	const newNoteRef = ref(null);	 
+
 	const notes = ref([
 		{
-			id: 1,
+			id: 'id1',
 			content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi praesentium saepe corrupti eaque non, hic repellat enim nulla officiis vitae ab expedita, architecto perspiciatis in accusamus sapiente harum laborum earum?'
 		},
 		{
-			id: 2,
+			id: 'id2',
 			content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
 		}
 	]);
+
+	const addNote = () =>{
+		let currentDate = new Date().getTime(),
+				id = currentDate.toString();
+		
+		let note = {
+			id: id,
+			content: newNote.value
+		};
+
+		notes.value.unshift(note);
+		newNote.value = '';
+		newNoteRef.value.focus();
+	}
 
 </script>
