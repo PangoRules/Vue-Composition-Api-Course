fcf5dc499382ee8b6edd29850aa1a64d61c73fb6
Rafael Bolaños <rafael.bolanos@thekag.com>
2023-12-21 20:03:41 -0500

    Tidying up the router set up

--- vue-composition-api-noteballs/src/main.js
+++ vue-composition-api-noteballs/src/main.js
@@ -1,26 +1,8 @@
-import './assets/main.css'
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from '@/router/index.js';
 
-import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import App from './App.vue'
-import ViewNotes from '@/views/ViewNotes.vue'
-import ViewStats from '@/views/ViewStats.vue'
-
-const router = createRouter({
-	history: createWebHashHistory(),
-	routes: [
-		{
-			path: '/',
-			name: 'notes',
-			component: ViewNotes
-		},
-		{
-			path: '/stats',
-			name: 'stats',
-			component: ViewStats
-		}
-	]
-});
 
 createApp(App)
 	.use(router)
